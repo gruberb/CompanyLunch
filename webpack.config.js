990c7cbe119b63b75c6bfefb468bd8579a0e20b6
Bastian Gruber <gruberbastian@me.com>
2015-11-28 20:17:24 +0100

    First working webpack-react-babel6 workflow

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  context: __dirname + "/app",
+  entry: {
+    javascript: "./app.js",
+    html: "./index.html",
+  },
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
