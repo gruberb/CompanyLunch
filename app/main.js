81c37cde3ec19ad700c6e06c057c88f321688a90
Bastian Gruber <gruberbastian@me.com>
2015-11-29 21:51:27 +0100

    Add strucutre for Flux architecture

--- app/main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App"
-
-ReactDOM.render(
-  <App txt="Hello World" cat={4}/>,
-  document.getElementById('app')
-);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'app/main.js'),
+  entry: path.resolve(__dirname, 'src/js/main.js'),
   output: {
-      path: path.resolve(__dirname, 'build'),
+      path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
+      publicPath: '/'
   },
   devtool: 'source-map',
   module: {
